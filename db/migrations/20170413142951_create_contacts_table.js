5b65b705b3d02f0c1b427e8c62172100b958a8f7
Amber Murray <amber.applebee@gmail.com>
2017-04-13 15:43:00 -0700

    created contacts table, contacts seed data

---
+++ db/migrations/20170413142951_create_contacts_table.js
@@ -0,0 +1,17 @@
+'use strict'
+
+exports.up = function(knex) {
+  return knex.schema.createTable('contacts', table => {
+    table.increments()
+    table.string('first').notNullable().defaultTo('')
+    table.string('last').notNullable().defaultTo('')
+    table.string('phone').notNullable().defaultTo(0)
+    table.string('email').notNullable().defaultTo('')
+    table.text('image')
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('contacts')
+}
---
+++ db/seeds/1_create_contact_seed_data.js
@@ -0,0 +1,50 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('contacts').del()
+  .then(function () {
+    // Inserts seed entries
+    return knex('contacts').insert([
+      {
+        first: 'Mary',
+        last:  'Crawley',
+        phone: '111-555-1212',
+        email: 'mcrawley@email.com',
+        image: 'http://www.radiotimes.com/uploads/images/Original/86703.jpg'
+      },
+      {
+        first: 'Violet',
+        last:  'Crawley',
+        phone: '333-542-1217',
+        email: 'lgrantham@email.com',
+        image: 'http://images6.fanpop.com/image/polls/1263000/1263632_1376371953914_full.jpg'
+      },
+      {
+        first: 'Cora',
+        last:  'Crawley',
+        phone: '888-555-1617',
+        email: 'ccrawley@email.com',
+        image: 'http://static.tvtropes.org/pmwiki/pub/images/cora_768.png'
+      },
+      {
+        first: 'Isobel',
+        last:  'Crawley',
+        phone: '777-890-1216',
+        email: 'mcrawley@email.com',
+        image: 'https://s-media-cache-ak0.pinimg.com/originals/12/65/f4/1265f4d99de2ecf93e80bc88a3885051.jpg'
+      },
+      {
+        first: 'Charles',
+        last:  'Carson',
+        phone: '123-345-1212',
+        email: 'mcarson@email.com',
+        image: 'http://2.bp.blogspot.com/__7s9GUTM-oY/TI6rTEaxuKI/AAAAAAAASP0/HWwmOxvCca8/s1600/da+12.PNG'
+      }
+
+    ]);
+  }).then (() => {
+    return knex.raw(
+      "SELECT setval('contacts_id_seq', (SELECT MAX(id) FROM contacts))"
+    )
+  })
+};
