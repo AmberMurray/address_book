0518110f032961b604502c21d0d01f64d0fa492d
Amber Murray <amber.applebee@gmail.com>
2017-04-14 12:52:31 -0700

    moved new form to partials folder, using as an edit form for new and update contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -13,6 +13,26 @@ router.get('/', (req, res, next) => {
   })
 })
 
+//show the add a new contact page
+router.get('/new', (req, res, next) => {
+  res.render('contacts/new')
+})
 
+//add a new contact & address
+router.post('/new', (req, res, next) => {
+  let newAddress = {
+    line_1: req.body.line_1,
+    line_2: req.body.line_2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip
+  }
+  knex('addresses')
+  .insert(newAddress, '*')
+  .then(address => {
+    console.log('AWWWWWW YEAH!', address)
+    res.render('contacts/index', { address })
+  })
+})
 
 module.exports = router
